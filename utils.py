a110de32014193ad0689ab4f111647e1e41a4b63
Sulu-Gambari Abdulrasaq <97162038+Razoidz@users.noreply.github.com>
2026-09-08 12:16:31 +0100

    Refactor expense calculation logic
    Removed unnecessary return statement and comments for clarity.

--- utils.py
+++ utils.py
@@ -7,10 +7,12 @@ def get_summary(transactions):
   for data in transactions:
     #iterate through each dictionary of expenses to get amount of each expense
     for amount in data.values():
-      #if the value of the amount is a number, add it to total expense and return total
+      #if the value of the amount is a number, add it to total expense
       if isinstance(amount, float) and not isinstance(amount, str):
         total_expense += amount
         #print(f"Total amount: {total_expense}")
+        #return total_expense
+    #calculate net balance remaining by deducting total_expense from total_income
     net_balance = total_income - total_expense
     #print(net_total)
     return net_balance
