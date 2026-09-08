2e18a9c781650999d752fadfb2142ee00b363ec5
randy433 <randycash47@gmail.com>
2026-09-08 11:02:30 +0100

    add_transaction

--- game.py
+++ game.py
@@ -1,8 +1,6 @@
-import datetime
 def add_transaction(transactions, type):
     description=input("Enter description: ")
     amount=float(input("Enter amount: "))
-    date= datetime.date.today().isoformat()
     if type == "income":
         source=input("Enter income source: ")
         new_record = {
@@ -10,7 +8,6 @@ def add_transaction(transactions, type):
             "description": description,
             "source": source,
             "amount": amount,
-            "date": date
         }
     else:
         category=input("Enter expense category: ")
@@ -19,7 +16,6 @@ def add_transaction(transactions, type):
             "description": description,
             "category": category,
             "amount": amount,
-            "date": date
         }
     transactions.append(new_record)
     print("Transaction added successfully")
