7abeeafce35d115d309026f33023cf4bfa51e2bf
Tolu Tim-Kolawole <jasminetolu74@gmail.com>
2026-09-09 11:35:28 +0100

    feat: Updated budget.py

--- budget.py
+++ budget.py
@@ -1,1 +1,28 @@
+import sys
+from utils import add_transaction, view_all, get_summary, view_by_category
+
 transactions = []
+type1 = str.lower("income")
+type2 = str.lower("expense")
+
+while True:
+    print("==== BUDGET TRACKER ====")
+    print("[1] Add Income  [2] Add Expense  [3] View All  [4] Summary  [5] By Category  [6] Exit")
+
+    user_input = input("> ")
+    try:
+        if user_input == "1":
+            add_transaction(transactions, type1)
+        elif user_input == "2":
+            add_transaction(transactions, type2)
+        elif user_input == "3":
+            view_all(transactions)
+        elif user_input == "4":
+            get_summary(transactions)
+        elif user_input == "5":
+            view_by_category(transactions)
+        elif user_input == "6":
+            sys.exit()
+    except ValueError:
+        print("Please enter a number from the given options.")
+        continue
